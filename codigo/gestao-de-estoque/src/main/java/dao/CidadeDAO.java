ae4e4d2c7dec26f5bb6e2f92fe63631ebe08a4be
Diogo-Miranda <kit02tempestade@hotmail.com>
2020-10-15 23:10:06 -0300

    data access object + tests (possui bugs)

---
+++ codigo/gestao-de-estoque/src/main/java/dao/CidadeDAO.java
@@ -0,0 +1,162 @@
+package dao;
+
+import java.sql.*;
+import java.util.List;
+import java.util.ArrayList;
+
+import model.Cidade;
+
+/**
+ * Class CidadeDAO - DAO Operates
+ * @author diogo
+ * Última atualização 15/10/2020
+ */
+public class CidadeDAO implements DAO<Cidade> {
+	
+	private Connection connection;
+	
+	public CidadeDAO() {
+		connection = null;
+	}
+	
+	/**
+	 * Open connection in DB
+	 * @return
+	 */
+	public boolean connect() {
+		String driverName = "org.postgresql.Driver";
+		String serverName = "localhost";
+		String myDB = "estoqueti2";
+		int porta = 5432;
+		String url = "jdbc:postgresql://" + serverName + ":" + porta + "/" + myDB;
+		String username = "estoqueti2";
+		String password = "123";
+		boolean status = false;
+		
+		try {
+			Class.forName(driverName);
+			connection = DriverManager.getConnection(url, username, password);
+			System.out.println("Conexão efetuada com o postgres!");
+		} catch (ClassNotFoundException e) {
+			System.err.println("Conexão NÃO efetuada com o banco --- Driver não encontrado -- " + e.getMessage());
+		} catch (SQLException e) {
+			System.err.println("Conexão NÃO efetuada com o postgres -- " + e.getMessage());
+		}	
+		
+		return status;
+	}
+	
+	/**
+	 * Close connection in DB
+	 * @return
+	 */
+	public boolean close() {
+		boolean status = false;
+		
+		try {
+			connection.close();
+			status = true;
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+		
+		return status;
+	}
+	
+    @Override
+	public Cidade get(int id) {
+		Cidade cidade = null;
+		try {
+			Statement st = connection.createStatement();
+			String sql = ("SELECT * "
+					    + "FROM cidade 	"
+					    + "WHERE cidade.id = " + id);
+			ResultSet rs = st.executeQuery(sql);
+				
+			cidade = new Cidade(rs.getInt("id"), 
+					 		    rs.getInt("codigo"), rs.getString("cidade"), rs.getString("cidade"));
+			st.close();
+			System.out.println("Success! --- " + cidade.toString());
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		// TODO Auto-generated method stub
+		return cidade;
+	}
+
+	@Override
+	public void add(Cidade cidade) {
+		try {
+			Statement st = connection.createStatement();
+			
+			// Pesquisar id válido
+			List<Cidade> cidades = getAll();
+			
+			// Evitar id Duplicado
+			int maiorId = 0;
+			for(Cidade c : cidades) {
+				if(c.getId() > maiorId) maiorId = c.getId();
+			}
+			
+			maiorId++;
+			
+			String sql = ("INSERT INTO cidade (id, codigo, cidade, uf) "
+					    + "VALUES (" + maiorId + ", " + cidade.getCodigo() + ", '" + cidade.getCidade() + "', '" + cidade.getUF()) + "')";
+			st.executeUpdate(sql);
+			System.out.println("Sucess! --- " + cidade.toString());
+		} catch (SQLException u) {
+			throw new RuntimeException(u);
+		}
+	}
+	
+	@Override
+	public void update(Cidade cidade) {
+		try {
+			Statement st = connection.createStatement();
+			String sql = ("UPDATE cidade SET codigo = " + cidade.getCodigo() + " , "
+					     + "cidade = '"+ cidade.getCidade() +"', uf = '"+ cidade.getUF() +"'"
+					     + "WHERE id = "+ cidade.getId());
+			st.executeUpdate(sql);
+			System.out.println("Sucess! --- " + cidade.toString());
+		} catch (SQLException u) {
+			throw new RuntimeException(u);
+		}
+	}
+
+	@Override
+	public void delete(Cidade cidade) {
+		try {
+			Statement st = connection.createStatement();
+			String sql = ("DELETE FROM cidade WHERE cidade = " + cidade.getCodigo());
+			st.executeUpdate(sql);
+			st.close();
+			System.out.println("Sucess! --- " + cidade.toString());
+		} catch (SQLException u) {
+			throw new RuntimeException(u);
+		}
+	}
+
+	@Override
+	public List<Cidade> getAll() {
+		List<Cidade> cidades = new ArrayList<Cidade>();
+		
+		try {
+			Statement st = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+			String sql = ("SELECT * FROM cidade");
+			ResultSet rs = st.executeQuery(sql);
+			if(rs.next()) {
+				rs.last();
+				Cidade cidade = new Cidade(rs.getInt("id"), 
+			 		    rs.getInt("codigo"), rs.getString("cidade"), rs.getString("cidade"));
+				rs.beforeFirst();
+				
+				cidades.add(cidade);
+			}
+			st.close();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		
+		return cidades;
+	}
+}
