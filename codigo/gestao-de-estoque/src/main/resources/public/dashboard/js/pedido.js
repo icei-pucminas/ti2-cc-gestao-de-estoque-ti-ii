41495527440e533ed5c281ff9ac405e0ca095f4d
dannielHCVieira <danniel.hcvieira@gmail.com>
2020-11-09 01:15:47 -0300

    Merge dashboards

---
+++ codigo/gestao-de-estoque/src/main/resources/public/dashboard/js/pedido.js
@@ -0,0 +1,50 @@
+// Função para compras
+document.querySelector(".bebida").addEventListener("click", (e) => {
+    let id = e.target.id;
+
+    console.log(id);
+    let bebidas = document.querySelectorAll(".bebida");
+
+    for(let i = 0; i < bebidas.length; i++) {
+        if(id == bebidas[i].value) {
+            //console.log(id);
+        }
+    }
+
+    event.preventDefault();
+    let codigo = document.querySelector(".codigo").textContent;
+    let nome = document.querySelector(".nome").textContent;
+    let volume = document.querySelector(".volume").textContent;
+    let fornecedor = document.querySelector(".fornecedor").textContent;
+    let quantidade = document.querySelector(".quantidade").textContent;
+    let preco = document.querySelector(".preco").textContent;
+    
+    let pedido = JSON.stringify({
+        codigo : codigo,
+        nome : nome,
+        volume : volume, 
+        fornecedor : fornecedor,
+        quantidade : quantidade,
+        preco : preco
+    });
+
+    //console.log(e.target.id);
+
+    localStorage.setItem("pedido", pedido);
+}); 
+
+function reqListener () {
+   let dado = JSON.parse(this.responseText);
+
+   console.log(dado);
+};
+
+let url = "http://localhost:6789/bebida";
+
+var xhr = new XMLHttpRequest();
+
+xhr.onload = reqListener;
+
+xhr.open('GET', url, true);
+xhr.send();
+
