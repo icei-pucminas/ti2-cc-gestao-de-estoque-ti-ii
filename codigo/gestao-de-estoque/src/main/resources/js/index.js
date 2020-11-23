9b176c5bbb02587a4600016e3de9551b84799e17
Diogo-Miranda <kit02tempestade@hotmail.com>
2020-11-23 15:34:42 -0300

    fix registro/loadIndex

---
+++ codigo/gestao-de-estoque/src/main/resources/js/index.js
@@ -0,0 +1,31 @@
+window.addEventListener( "load", function (event) {
+    let email = JSON.parse(localStorage.getItem('email'));
+
+    loadDashboard( email );
+
+    function loadDashboard ( email ) {
+        let xhr = new XMLHttpRequest();
+
+        xhr.addEventListener( 'load', function ( event ) {
+            loadUser(event.target.responseText);
+        } );
+
+        // Define case error
+        xhr.addEventListener( ' error', function ( event ) {
+            alert ( 'Oops! ' );
+        } );
+
+        xhr.open( "GET", `http://localhost:6789/user/${email}`);
+
+        xhr.send();
+    }   
+
+    function loadUser ( user ) {
+        localStorage.setItem("user", user);
+        let objUser = JSON.parse(user);
+
+        console.log(objUser);
+
+        document.getElementById("nomeUsuario").innerText = `Olá, ${objUser.nome} ${objUser.sobrenome}`;
+    }
+});
