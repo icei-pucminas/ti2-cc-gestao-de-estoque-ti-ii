8215f44f10931171df1f404b957699a5343aca3c
Danniel H.C. Vieira <danniel.hcvieira@gmail.com>
2020-11-24 14:40:18 -0300

    Fixed paths in index.html

--- codigo/gestao-de-estoque/src/main/java/service/PedidoService.java
+++ codigo/gestao-de-estoque/src/main/java/service/PedidoService.java
@@ -1,47 +1,3 @@
-<<<<<<< HEAD
-package service;
-
-import java.io.IOException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
-import org.json.JSONArray;
-
-import dao.PedidoDAO;
-import model.Bebida;
-import model.Pedido;
-import spark.Request;
-import spark.Response;
-
-public class PedidoService {
-	private PedidoDAO pedidoDAO;
-	
-	public PedidoService() {
-		pedidoDAO = new PedidoDAO();
-	}
-	
-	public Object add(Request request, Response response) {
-		pedidoDAO.connect();
-		int codigo = Integer.parseInt(request.queryParams("bebidaCodigo"));
-		String nome = request.queryParams("bebidaNome");
-		String descricao = request.queryParams("bebidaDescricao");
-		float volume = Float.parseFloat(request.queryParams("bebidaVolume"));
-		int quantidade = Integer.parseInt(request.queryParams("bebidaQuantidade"));
-		//int idFornecedor = Integer.parseInt(request.queryParams("idFornecedor"));
-		
-		// Provisório
-		int idFornecedor = 3;
-		
-		Bebida bebida = new Bebida(codigo, nome, descricao, volume, quantidade, idFornecedor);
-
-		//bebidaDAO.add(bebida);
-
-		response.status(201); // created
-
-		return Integer.valueOf(codigo);
-	}
-}
-=======
 package service;
 
 import java.time.LocalDateTime;
@@ -184,5 +140,3 @@ public class PedidoService implements Service{
 	}
 	
 }
-
->>>>>>> dev/back-end
