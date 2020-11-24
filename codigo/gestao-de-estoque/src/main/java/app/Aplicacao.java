9bb4f179a737bba10eaafea41ef6784305a9087d
Danniel H.C. Vieira <danniel.hcvieira@gmail.com>
2020-11-24 14:17:15 -0300

    Fixed aplicacao from merge

--- codigo/gestao-de-estoque/src/main/java/app/Aplicacao.java
+++ codigo/gestao-de-estoque/src/main/java/app/Aplicacao.java
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-package app;
-
-import static spark.Spark.*;
-import service.*;
-
-
-public class Aplicacao {
-	
-	private static BebidaService bebidaService = new BebidaService();
-	private static UserService userService = new UserService();
-	
-	public static void main(String[] args) {
-		port(6789);
-		
-		post( "/bebida",     (request, response) ->  bebidaService.add(request, response) );
-		get(  "/bebida",     (request, response) ->  bebidaService.getAll(request, response) );
-			
-		get ( "/user/:email", (request, response) ->  userService.get(request, response));
-		post( "/user",       (request, response) ->  userService.add(request, response));
-	}
-	
-}
-=======
 package app;
 
 import static spark.Spark.*;
@@ -87,4 +63,3 @@ public class Aplicacao {
 	}
 	
 }
->>>>>>> dev/back-end
